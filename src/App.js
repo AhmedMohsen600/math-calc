8bd3af1dd39516bd6fe91b2b676cc291e211fa8e
AhmedMohsen600 <ahmedmohsen159357@gmail.com>
2022-11-28 16:39:16 +0200

    resolve linters errors

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>Ahmed</p>
         <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           Learn React
         </a>
