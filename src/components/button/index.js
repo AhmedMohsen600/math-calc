a8ad91804bbdd8b2d0c5d2fe12d339ed7ed9f224
AhmedMohsen600 <ahmedmohsen159357@gmail.com>
2022-12-01 18:40:18 +0200

    Refactor class component to functioanl component and hooks

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,22 +1,17 @@
-import { PureComponent } from 'react';
-
 import './styles/button.css';
 
-class Button extends PureComponent {
-  render() {
-    const { className, handelClick, mark } = this.props;
-    return (
-      <button
-        onClick={() => {
-          handelClick(mark);
-        }}
-        className={`${className}`}
-        type="button"
-      >
-        {mark}
-      </button>
-    );
-  }
-}
+const Button = ({ className, handelClick, mark }) => {
+  return (
+    <button
+      onClick={() => {
+        handelClick(mark);
+      }}
+      className={`${className}`}
+      type="button"
+    >
+      {mark}
+    </button>
+  );
+};
 
 export default Button;
